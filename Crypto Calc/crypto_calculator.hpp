ab0303fce2371dec327527e243e44b0a2b1a2d69
Arjun <YOUR_EMAIL>
2025-02-12 21:05:31 -0800

    Project structure Redone

--- Crypto Calc/crypto_calculator.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CRYPTO_CALCULATOR_HPP
-#define CRYPTO_CALCULATOR_HPP
-
-// Function declarations
-double calculateProfitLoss(double investment, double buyPrice, double sellPrice, int numCoins);
-void displayResults(double profitLoss);
-
-#endif
