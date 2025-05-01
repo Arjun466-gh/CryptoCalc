ca9bc372e611432974b9c34a870781f732070b51
Arjun <YOUR_EMAIL>
2025-04-30 22:09:18 -0700

    Cleaning Project 1 Files and rebuilding

--- Crypto Calc/include/crypto_calculator.hpp
+++ Crypto Calc/include/crypto_calculator.hpp
@@ -1,13 +1,1 @@
-#ifndef CRYPTO_CALCULATOR_HPP
-#define CRYPTO_CALCULATOR_HPP
-
-#include <string>
-#include <map>
-
-class CryptoCalculator {
-public:
-    double convertCurrency(const std::string& from, const std::string& to, double amount);
-    void displayRate(const std::string& currency);
-};
-
-#endif
+error_category
--- Crypto Calc/include/market_simulation.hpp
+++ Crypto Calc/include/market_simulation.hpp
@@ -1,12 +1,1 @@
-#ifndef MARKET_SIMULATION_HPP
-#define MARKET_SIMULATION_HPP
-
-#include <vector>
-
-// Function to load historical price data from a file
-std::vector<double> loadHistoricalData();
-
-// Function to calculate the moving average of given price data
-double calculateMovingAverage(const std::vector<double>& prices);
-
-#endif
+error 
