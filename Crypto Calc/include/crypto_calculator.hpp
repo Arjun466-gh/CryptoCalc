de477d831d6c9ee563af8a6d8196f1e7d1d21df6
Arjun <YOUR_EMAIL>
2025-04-30 22:27:24 -0700

    Added crypto_calculator.hpp with function declarations for profit/loss calculation

--- Crypto Calc/include/crypto_calculator.hpp
+++ Crypto Calc/include/crypto_calculator.hpp
@@ -1,1 +1,15 @@
-error_category
+#ifndef CRYPTO_CALCULATOR_HPP
+#define CRYPTO_CALCULATOR_HPP
+
+#include "transaction.hpp"
+#include <vector>
+#include <memory>
+
+double calculateProfitLoss(double investment, double buyPrice, double sellPrice, int numCoins);
+void displayResults(double profitLoss);
+
+//Using smart pointer for possible transaction tracking
+double calculatePortfolioValue(const std::vector<std::shared_ptr<Transaction>>& transactions);
+
+#endif 
+
