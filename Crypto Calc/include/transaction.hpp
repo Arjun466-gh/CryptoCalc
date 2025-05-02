462d539b3173518edadf55b245ad6a476b46a0c2
Arjun <YOUR_EMAIL>
2025-05-01 17:28:31 -0700

    Updated transcation files

--- Crypto Calc/include/transaction.hpp
+++ Crypto Calc/include/transaction.hpp
@@ -2,15 +2,27 @@
 #define TRANSACTION_HPP
 
 #include <string>
+#include <iostream>
 
 enum class TransactionType { BUY, SELL };
 
 struct Transaction {
-    std::string coinName;
-    TransactionType type;
-    double pricePerCoin;
+    std::string coin;
     int quantity;
-    std::string timestamp;
+    double pricePerCoin;
+    TransactionType type;
+
+    
+    Transaction(const std::string& coinName, int qty, double price, TransactionType t)
+        : coin(coinName), quantity(qty), pricePerCoin(price), type(t) {}
+
+    void print() const {
+        std::cout << "Coin: " << coin
+                  << ", Quantity: " << quantity
+                  << ", Price: $" << pricePerCoin
+                  << ", Type: " << (type == TransactionType::BUY ? "Buy" : "Sell")
+                  << std::endl;
+    }
 };
 
 #endif 
