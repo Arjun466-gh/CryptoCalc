90ee6ce7ea6f0fdfb674583872fd8b2caa02cbf1
Arjun <YOUR_EMAIL>
2025-05-01 17:07:23 -0700

    Market stimulation implemented in cpp file

--- Crypto Calc/src/market_simulation.cpp
+++ Crypto Calc/src/market_simulation.cpp
@@ -3,4 +3,7 @@
 
 double simulateMarketChange(double basePrice) {
     std::random_device rd;
- 
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-0.15, 0.15);
+    return basePrice * (1 + dis(gen));
+}
